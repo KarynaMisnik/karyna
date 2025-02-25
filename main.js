a600bdaaecd316ae225f8700eeea990480f9bb3c
KarynaMisnik <karinamisnik94@gmail.com>
2025-02-25 20:07:12 +0200

    img character sprite frame fixes
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ const character = document.getElementById("character");
 const startButton = document.getElementById("start-btn");
 const dialogue = document.getElementById("dialogue");
 
+character.addEventListener("mouseover", () => {
+  character.style.animation = "animate-wave 0.5s steps(4) infinite"; // Play waving animation
+});
+
+character.addEventListener("mouseleave", () => {
+  character.style.animation = "animate-idle 1.5s steps(4) infinite"; // Back to idle breathing
+});
+
 /*
 startButton.addEventListener("mouseover", () => {
   character.style.backgroundImage = "url('character-wave.png')";
